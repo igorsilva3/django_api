e08eeb13d6a53bde8a78cd4d9bac48204acb7fa1
igorsilva3 <igorbr2018@gmail.com>
2021-04-25 01:01:30 -0300

    Create product model

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value', 'created_date', 'updated_date')
+    ordering = ['name']
+    search_fields = ['name']
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	"""Model definition for Product."""
+
+	id = models.AutoField(primary_key=True)
+	name = models.CharField("Name", max_length=60, blank=False)
+	value = models.FloatField("Value")
+	created_date = models.DateField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		"""Meta definition for Product."""
+		verbose_name = 'Product'
+		verbose_name_plural = 'Products'
+
+	def __str__(self):
+		"""Unicode representation of Product."""
+		return self.name
